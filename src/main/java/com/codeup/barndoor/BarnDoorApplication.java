3ba234039237914e90dc092b33138ef1c0996f19
Chase Martinez <chasemartinez22@gmail.com>
2022-08-11 14:33:10 -0500

    Adding basic repository files

---
+++ src/main/java/com/codeup/barndoor/BarnDoorApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.barndoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarnDoorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarnDoorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/barndoor/BarnDoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.barndoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarnDoorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
