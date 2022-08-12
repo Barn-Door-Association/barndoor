031198b8298c7cd03b78e1fc22354d8494134225
Chase Martinez <chasemartinez22@gmail.com>
2022-08-12 11:57:35 -0500

    Feat: add User entity

--- src/main/java/com/codeup/barndoor/models/User.java
+++ src/main/java/com/codeup/barndoor/models/User.java
@@ -1,6 +1,110 @@
 package com.codeup.barndoor.models;
 
+import javax.persistence.*;
+
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String ranchName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    // Add relationship to herd @
+
+    public User() {
+    }
+
+    public User(User copy) {
+        this.id = copy.id;
+        this.username = copy.username;
+        this.password = copy.password;
+        this.email = copy.email;
+        this.ranchName = copy.ranchName;
+        this.firstName = copy.firstName;
+        this.lastName = copy.lastName;
+    }
+
+    public User(long id, String username, String password, String email, String ranchName, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.ranchName = ranchName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRanchName() {
+        return ranchName;
+    }
+
+    public void setRanchName(String ranchName) {
+        this.ranchName = ranchName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
