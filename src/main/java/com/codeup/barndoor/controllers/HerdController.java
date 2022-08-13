bbe4b5d3c0cc360499a468a10099d18a4c0d9c9c
Chase Martinez <chasemartinez22@gmail.com>
2022-08-13 15:29:03 -0500

    Added thymeleaf to html, added code allowing of objects ability tot pass from java to html. added Javacsript to test buttons communication from site

--- src/main/java/com/codeup/barndoor/controllers/HerdController.java
+++ src/main/java/com/codeup/barndoor/controllers/HerdController.java
@@ -1,13 +1,32 @@
 package com.codeup.barndoor.controllers;
 
+import com.codeup.barndoor.models.Goat;
+import com.codeup.barndoor.models.Herd;
+import com.codeup.barndoor.repositories.HerdRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class HerdController {
 
-    @GetMapping("/herd")
-    public String showHerdPage(){
-        return "herd";
+    HerdRepository herdDao;
+
+    @GetMapping("/herds")
+    public String showHerds(Model model){
+        List<Herd> herds = herdDao.findAll();
+        model.addAttribute("herds", herds);
+        return "herds/herds";
+    }
+
+    @GetMapping("/herd/{id}")
+    public String showHerdPage(@PathVariable long id, Model model){
+//         This will not work unless tables have data in them
+         List<Goat> herd = herdDao.findById(id).getGoats();
+         model.addAttribute("herd", herd);
+        return "herds/herd";
     }
 }
