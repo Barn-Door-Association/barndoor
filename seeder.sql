7670d7f593f87aba2400750f805dfe2050ade199
Nia Watson <nia.d.watson@gmail.com>
2022-08-15 10:56:22 -0500

    feat. Was able to fix foreign key issues on herds. Getting error for vaccination

--- seeder.sql
+++ seeder.sql
@@ -7,36 +7,56 @@
 #
 # SET foreign_key_checks = 0;
 # DROP TABLE IF EXISTS herds;
-# SET foreign_key_checks = 1;
+
 USE barndoor_db;
+# # SET foreign_key_checks = 0;
+# # TRUNCATE goats;
+SET foreign_key_checks = 1;
+# # insert into herds (herd_name) VALUES ('Boys'),
+# #                                      ('Older Ladies'),
+# #                                      ('Young Ladies');
+insert into goats (tag_id, name, dob, sex, breed, weight_in_pounds, herd_id) VALUES (221, 'Latte', '2019-03-19', 'F', 'Boer/Kiko/Nubian/LaMancha/Spanish', 92, 2),
+                                                                           (218, 'Martinie', '2018-03-17', 'F', 'Boer/Kiko/Nubian/Spanish', 85, 2),
+                                                                           (219, 'Paperbag', '2018-04-19', 'F', 'Boer/Kiko/Nubian/Spanish', 80, 2),
+                                                                           (220, 'Rubicon', '2018-05-24', 'F', 'Boer/Kiko/Spanish', 95, 2),
+                                                                           (222, 'Bobe', '2021-04-16', 'M', 'Boer/Kiko/Nubian/LaMancha/Spanish', 5, 1),
+                                                                           (223, 'Pete', '2021-04-16', 'M', 'Boer/Kiko/Nubian/LaMancha/Spanish', 50, 1),
+                                                                           (224, 'Lilly', '2021-04-16', 'F', 'Boer/Kiko/Nubian/LaMancha/Spanish', 50, 2),
+                                                                           (225, 'Oreo', '2022-02-22', 'M', 'Boer/Kiko/Nubian/LaMancha/Spanish', 25, 1),
+                                                                           (226, 'Cookie', '2022-02-22', 'M', 'Boer/Kiko/Nubian/LaMancha/Spanish', 25, 1),
+                                                                           (227, 'Nana', '2021-04-27', 'F', 'Boer/Kiko/Nubian/Spanish/LaMancha', 50, 2),
+                                                                           (228, 'PoPo', '2021-04-27', 'M', 'Boer/Kiko/Nubian/Spanish/LaMancha', 50, 1),
+                                                                           (229, 'Shadrach', '2022-05-13', 'M', 'Boer/Kiko/Nubian/Spanish/LaMancha', 15, 1),
+                                                                           (230, 'Meshach', '2022-05-13', 'M', 'Boer/Kiko/Nubian/Spanish/LaMancha', 15, 1),
+                                                                           (231, 'Abednego', '2022-05-13', 'M', 'Boer/Kiko/Nubian/Spanish/LaMancha', 15, 1),
+                                                                           (232, 'Durango', '2021-04-13', 'M', 'Boer/Kiko/Spanish/LaMancha', 50, 1),
+                                                                           (233, 'HiLo', '2021-04-13', 'F', 'Boer/Kiko/Spanish/LaMancha', 50, 2),
+                                                                           (234, 'Dior', '2022-05-26', 'F', 'Boer/Kiko/Spanish/LaMancha', 15, 3),
+                                                                           (235, 'Lauren', '2022-05-22', 'F', 'Boer/Kiko/Spanish/LaMancha', 15, 3);
 
-# insert into herds (herd_name) VALUES ('Boys'),
-#                                      ('Older Ladies'),
-#                                      ('Young Ladies');
+# insert into vaccines (description, vaccine_name) VALUES ('pneumonia', 'Presponse HM'),
 
-insert into goats (tag_id, name, dob, sex, breed, weight_in_pounds) VALUES (221, 'Latte', '2019-03-19', 'F', 'Boer/Kiko/Nubian/LaMancha/Spanish', 92),
-                                                                           (218, 'Martinie', '2018-03-17', 'F', 'Boer/Kiko/Nubian/Spanish', 85),
-                                                                           (219, 'Paperbag', '2018-04-19', 'F', 'Boer/Kiko/Nubian/Spanish', 80),
-                                                                           (220, 'Rubicon', '2018-05-24', 'F', 'Boer/Kiko/Spanish', 95),
-                                                                           (222, 'Bobe', '2021-04-16', 'M', 'Boer/Kiko/Nubian/LaMancha/Spanish', 5),
-                                                                           (223, 'Pete', '2021-04-16', 'M', 'Boer/Kiko/Nubian/LaMancha/Spanish', 50),
-                                                                           (224, 'Lilly', '2021-04-16', 'F', 'Boer/Kiko/Nubian/LaMancha/Spanish', 50),
-                                                                           (225, 'Oreo', '2022-02-22', 'M', 'Boer/Kiko/Nubian/LaMancha/Spanish', 25),
-                                                                           (226, 'Cookie', '2022-02-22', 'M', 'Boer/Kiko/Nubian/LaMancha/Spanish', 25),
-                                                                           (227, 'Nana', '2021-04-27', 'F', 'Boer/Kiko/Nubian/Spanish/LaMancha', 50),
-                                                                           (228, 'PoPo', '2021-04-27', 'M', 'Boer/Kiko/Nubian/Spanish/LaMancha', 50),
-                                                                           (229, 'Shadrach', '2022-05-13', 'M', 'Boer/Kiko/Nubian/Spanish/LaMancha', 15),
-                                                                           (230, 'Meshach', '2022-05-13', 'M', 'Boer/Kiko/Nubian/Spanish/LaMancha', 15),
-                                                                           (231, 'Abednego', '2022-05-13', 'M', 'Boer/Kiko/Nubian/Spanish/LaMancha', 15),
-                                                                           (232, 'Durango', '2021-04-13', 'M', 'Boer/Kiko/Spanish/LaMancha', 50),
-                                                                           (233, 'HiLo', '2021-04-13', 'F', 'Boer/Kiko/Spanish/LaMancha', 50),
-                                                                           (234, 'Dior', '2022-05-26', 'F', 'Boer/Kiko/Spanish/LaMancha', 15),
-                                                                           (235, 'Lauren', '2022-05-22', 'F', 'Boer/Kiko/Spanish/LaMancha', 15),
-                                                                           (236, 'Ralph', '2022-05-22', 'M', 'Boer/Kiko/Spanish/LaMancha', 95),
-                                                                           (237, 'Morse II', '2022-05-22', 'M', 'Myotonic', 120);
-
-insert into vaccines (description, vaccine_name) VALUES ('pneumonia', 'Presponse HM'),
-                                                        ('provides long-term protection against overeating disease (types C&D) and tetanus.', 'CD/T');
+insert into vaccine_records (date_administered, dosage_in_ccs, is_booster, goat_id, vaccines_id) VALUES ('2022-07-02', 2, 1, 20, 2),
+                                                                                                        ('2022-07-02', 2, 1, 8, 2),
+                                                                                                        ('2022-07-02', 2, 1, 9, 2),
+                                                                                                        ('2022-07-02', 1, 1, 8, 1),
+                                                                                                        ('2022-07-02', 1, 1, 9, 1),
+                                                                                                        ('2022-07-02', 2, 0, 7, 1),
+                                                                                                        ('2022-07-02', 2, 0, 10, 1),
+                                                                                                        ('2022-07-02', 2, 0, 7, 2),
+                                                                                                        ('2022-07-02', 2, 0, 10, 2),
+                                                                                                        ('2022-07-02', 1, 0, 12, 1),
+                                                                                                        ('2022-07-02', 1, 0, 13, 1),
+                                                                                                        ('2022-07-02', 1, 0, 14, 1),
+                                                                                                        ('2022-07-02', 1, 0, 17, 1),
+                                                                                                        ('2022-07-02', 1, 0, 18, 1),
+                                                                                                        ('2022-07-02', 1, 0, 19, 1),
+                                                                                                        ('2022-07-02', 2, 0, 12, 2),
+                                                                                                        ('2022-07-02', 2, 0, 13, 2),
+                                                                                                        ('2022-07-02', 2, 0, 14, 2),
+                                                                                                        ('2022-07-02', 2, 0, 17, 2),
+                                                                                                        ('2022-07-02', 2, 0, 18, 2),
+                                                                                                        ('2022-07-02', 2, 0, 19, 2);
 
 
 
