b95ac3a779f32a635df51d6986cce89f6729bbcf
Chase Martinez <chasemartinez22@gmail.com>
2022-08-17 11:21:47 -0500

    Created VaccineRecordsRepository and VaccineRepository interfaces

---
+++ src/main/java/com/codeup/barndoor/repositories/VaccineRepository.java
@@ -0,0 +1,8 @@
+package com.codeup.barndoor.repositories;
+
+import com.codeup.barndoor.models.Vaccine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
+    Vaccine findbyId (long id);
+}
