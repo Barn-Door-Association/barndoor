52b94770186bc25e97a32562c8172f0fc6a23af5
Leonard Contreras <leonard.contreras03@gmail.com>
2022-08-22 12:23:43 -0500

    mapped to pedigree page

--- src/main/java/com/codeup/barndoor/controllers/GoatController.java
+++ src/main/java/com/codeup/barndoor/controllers/GoatController.java
@@ -59,7 +59,7 @@ public class GoatController {
     }
 
     @GetMapping("/goats/{id}/pedigree")
-    public String showPedigree(Model model) {
-        return "pedigree";
+    public String showPedigree(@PathVariable long id) {
+        return "goats/pedigree";
     }
 }
